d15e0555768ae2cc4a02880d9c58a12e16ca6cc6
calyco2022 <gd1.calyco@gmail.com>
2023-08-19 14:06:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
+import Carausel from './Components/Carausel';
+import Productlist from './Components/Productlist';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Carausel/>
+      <Productlist/>
      <Footer/>
     </div>
   );
